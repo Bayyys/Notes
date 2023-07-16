7de1a71de47f045f33cf73d9d4d745e6c113d2b0
bayyy <475417309@qq.com>
2023-07-16 15:05:42 +0800

    📝MyBatis: CRUD
    1.Chapter-3 CRUD
    
    bayyy-23.07.16

--- Mybatis/mybatis-study/mybatis-01/src/main/java/com/bayyy/dao/UserMapper.java
+++ Mybatis/mybatis-study/mybatis-01/src/main/java/com/bayyy/dao/UserMapper.java
@@ -3,7 +3,24 @@ package com.bayyy.dao;
 import com.bayyy.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
+    // 查询全部用户
     List<User> getUserList();
+
+    // 根据id查询用户
+    User getUserById(int id);
+
+    // 插入一个用户
+    int addUser(User user);
+
+    // Map 方式修改一个用户
+    int mapUpdate(Map<String, Object> map);
+
+    // 修改一个用户
+    int updateUser(User user);
+
+    // 删除一个用户
+    int deleteUser(int id);
 }
--- Mybatis/mybatis-study/mybatis-01/src/test/java/com/bayyy/dao/UserMapperTest.java
+++ Mybatis/mybatis-study/mybatis-01/src/test/java/com/bayyy/dao/UserMapperTest.java
@@ -5,20 +5,16 @@ import com.bayyy.utils.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest {
     @Test
-    public void test() {
+    public void testSelectAll() {
         // try-with-resources: try内部的资源会自动释放(且是先声明的资源先释放)
         try (SqlSession sqlSession = MyBatisUtils.getSqlSession()) {
-            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-            List<User> userList = userMapper.getUserList();
-            for (User user : userList) {
-                System.out.println(user);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            sqlSession.getMapper(UserMapper.class).getUserList().forEach(System.out::println);
         }
 //        // 1. 获取sqlSession对象
 //        SqlSession sqlSession = MyBatisUtils.getSqlSession();
@@ -38,5 +34,69 @@ public class UserMapperTest {
 //
 //        // 3. 关闭sqlSession
 //        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectById() {
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSession()) {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            User user = mapper.getUserById(1);
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testAdd() {
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSession()) {
+            int add = sqlSession.getMapper(UserMapper.class).addUser(new User(4, "长江四号", "123456"));
+            if (add > 0) {
+                User user = sqlSession.getMapper(UserMapper.class).getUserById(4);
+                System.out.println(user);
+            } else {
+                System.out.println("插入失败");
+            }
+            sqlSession.commit();
+        }
+    }
+
+    @Test
+    public void testDelete() {
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSession()) {
+            int delete = sqlSession.getMapper(UserMapper.class).deleteUser(4);
+            if (delete > 0) {
+                System.out.println("删除成功");
+                sqlSession.getMapper(UserMapper.class).getUserList().forEach(System.out::println);
+            } else {
+                System.out.println("删除失败");
+            }
+            sqlSession.commit();
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSession()) {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            int update = mapper.updateUser(new User(4, "长江四号", "654321"));
+            if (update > 0) {
+                System.out.println("修改成功");
+                User user = mapper.getUserById(4);
+                System.out.println(user);
+            } else {
+                System.out.println("修改失败");
+            }
+            sqlSession.commit();
+        }
+    }
+
+    @Test
+    public void testMapUpdate() {
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSession()) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("mapId", 4);
+            map.put("mapName", "黄河四号");
+            System.out.println(sqlSession.getMapper(UserMapper.class).mapUpdate(map));
+            sqlSession.commit();
+        }
     }
 }
