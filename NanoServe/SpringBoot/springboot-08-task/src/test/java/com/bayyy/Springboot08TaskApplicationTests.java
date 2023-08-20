4cf9532bbc8572fb04b2d15f432eed3a64a8796e
BAY <475417309@qq.com>
2023-08-21 01:07:32 +0800

    📝 SpringCloud: Eureka*
    1. Eureka 服务注册与发现
    
    BAY-23.08.20

--- NanoServe/SpringBoot/springboot-08-task/src/test/java/com/bayyy/Springboot08TaskApplicationTests.java
+++ NanoServe/SpringBoot/springboot-08-task/src/test/java/com/bayyy/Springboot08TaskApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
---
+++ NanoServe/SpringCloud/springcloud-eureka-7001/src/main/java/com/bayyy/springcloud/EurekaServer_7001.java
@@ -0,0 +1,13 @@
+package com.bayyy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer     // EurekaServer 服务器端启动类，接受其他微服务注册进来
+public class EurekaServer_7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class, args);
+    }
+}
