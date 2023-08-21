f2ade702a3adf1506f7aa71134fd5eb163ff0a65
BAY <475417309@qq.com>
2023-08-22 01:42:32 +0800

    📝 SpringCloud: Eureka*
    1. Eureka 服务注册与发现
    
    BAY-23.08.22

--- NanoServe/SpringCloud/springcloud-provider-dept-8001/src/main/java/com/bayyy/springcloud/DeptProvider_8001.java
+++ NanoServe/SpringCloud/springcloud-provider-dept-8001/src/main/java/com/bayyy/springcloud/DeptProvider_8001.java
@@ -1,14 +1,17 @@
 package com.bayyy.springcloud;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 // 启动类
 @SpringBootApplication()
+@EnableDiscoveryClient // 开启服务发现
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
--- NanoServe/SpringCloud/springcloud-provider-dept-8001/src/main/java/com/bayyy/springcloud/controller/DeptController.java
+++ NanoServe/SpringCloud/springcloud-provider-dept-8001/src/main/java/com/bayyy/springcloud/controller/DeptController.java
@@ -3,6 +3,7 @@ package com.bayyy.springcloud.controller;
 import com.bayyy.springcloud.pojo.Dept;
 import com.bayyy.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
