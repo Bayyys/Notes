3876b0a095f71667780291d66c6bcc8b0bc5bef0
bayyy <475417309@qq.com>
2023-08-24 14:26:06 +0800

    📝 SpringCloud: Hystrix：服务熔断*
    bayyy-23.08.24

---
+++ NanoServe/SpringCloud/springcloud-provider-dept-hystrix-8001/src/main/java/com/bayyy/springcloud/DeptProvider_Hystrix_8001.java
@@ -0,0 +1,15 @@
+package com.bayyy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// 启动类
+@SpringBootApplication()
+@EnableDiscoveryClient // 开启服务发现
+
+public class DeptProvider_Hystrix_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_Hystrix_8001.class, args);
+    }
+}
