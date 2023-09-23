be83fc863761b6b9ba86dadd4591a2e856fa2f6f
BAY <475417309@qq.com>
2023-09-24 02:50:22 +0800

    📝 Vue3: getters配置项
    1. 概念和应用 、Vuex工作原理 、搭建vuex环境 、vuex使用 、vuex开发者工具
    2. getters配置项
    
    BAY-23.09.24

---
+++ FrontWeb/Vue3/4_vuex/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <hr>
+    <Count></Count>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue'
+export default {
+  name: 'App',
+  components: { Count },
+  data() {
+    return {
+      msg: 'Hello World!',
+    }
+  },
+}
+</script>
+
+<style>
+button {
+  margin-left: 5px;
+}
+</style>
---
+++ FrontWeb/Vue3/4_vuex/纯vue求和案例/components/Count.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>当前求和为: {{ sum }}</h1>
+        <select v-model.number="n">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select> <br>
+        <button @click="increment">add</button>
+        <button @click="decrement">sub</button>
+        <button @click="incOdd">add if odd</button>
+        <button @click="incWait">add wait</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Count',
+    data() {
+        return {
+            n: 1,   // 选择的数字
+            sum: 0, // 求和结果
+
+        }
+    },
+    methods: {
+        increment() {
+            this.sum += this.n
+        },
+        decrement() {
+            this.sum -= this.n
+        },
+        incOdd() {
+            if(this.sum % 2) this.sum += this.n
+        },
+        incWait() {
+            setTimeout(() => {
+                this.sum += this.n
+            }, 1000)
+        },
+    }
+}
+</script>
+
+<style></style>
