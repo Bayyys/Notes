86e4fb1213769b2398757e553005a4ca71a832ca
bayyy <475417309@qq.com>
2023-09-23 15:59:38 +0800

    🏁 Vue3: Vue组件化开发完成
    bayyy-23.09.23

---
+++ FrontWeb/Vue3/3_Ajax/vue2_component/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+}
+
+</script>
+
+<style></style>
