1598b3e85c2f593735e0ca5c3999cbcfee459914
BAY <475417309@qq.com>
2023-09-29 00:15:09 +0800

    🎉 AJAX
    BAY-23.09.29

---
+++ FrontWeb/Ajax/3_其他AJAX请求发送/server.js
@@ -0,0 +1,72 @@
+// 1. 引入express模块
+const express = require("express");
+
+// 2. 创建express实例
+const app = express();
+
+// 3. 创建路由规则
+// request是对请求报文的封装
+// response是对响应报文的封装
+app.get("/server", (request, response) => {
+  // 设置响应头，设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  // 设置响应体
+  response.send("Hello AJAX");
+  console.log("请求来自于", request.get("Host"));
+  console.log("请求的地址", request.url);
+});
+
+app.post("/server-json", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.send("Hello AJAX(POST)");
+  console.log("请求来自于", request.get("Host"));
+  console.log("请求的地址", request.url);
+});
+
+app.get("/ie", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.send("Hello IE");
+  console.log("请求来自于", request.get("Host"));
+  console.log("请求的地址", request.url);
+});
+
+app.get("/timeout", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  setTimeout(() => {
+    response.send("Hello Timeout ");
+    console.log("请求来自于", request.get("Host"));
+    console.log("请求的地址", request.url);
+  }, 2000);
+});
+
+app.all("/jquery-server", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  const data = { name: "admin" };
+  response.send(JSON.stringify(data));
+  console.log("请求来自于", request.get("Host"));
+  console.log("请求的地址", request.url);
+});
+
+app.all("/axios-server", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  const data = { name: "admin" };
+  response.send(JSON.stringify(data));
+  console.log("请求来自于", request.get("Host"));
+  console.log("请求的地址", request.url);
+});
+
+app.all("/fetch-server", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  const data = { name: "admin" };
+  response.send(JSON.stringify(data));
+  console.log("请求来自于", request.get("Host"));
+  console.log("请求的地址", request.url);
+});
+
+// 4. 监听端口启动服务
+app.listen(8000, () => {
+  console.log("服务已经启动, 8000端口监听中...");
+});
