0d6f30199d4daa4331e28cb6f6a15ed78ad93486
BAY <475417309@qq.com>
2023-09-30 23:51:04 +0800

    📝 Node.js: 包管理工具
    BAY-23.09.30

---
+++ WebTools/Nodejs/7_包管理工具/01_npm/index.js
@@ -0,0 +1,10 @@
+const uniq = require("uniq");
+const math = require("math_bayyy");
+
+var arr = [1, 3, 2, 1, 4, 2, 3, 5, 4];
+
+const result = uniq(arr);
+console.log(math.add(1, 2));
+console.log(math.minus(1, 2));
+
+console.log(result);
---
+++ WebTools/Nodejs/7_包管理工具/04_publish_package/index.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log("bayyy create");
+module.exports = { add, minus, multiply, divide };
