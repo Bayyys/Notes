26627df0fb3b85c972770555984eff4f0c5014d4
bayyy <475417309@qq.com>
2023-10-10 21:59:31 +0800

    📝 Webpack: 基础完成
    bayyy-23.10.10

--- FrontWeb/Webpack/1_基础/01_基本使用/src/main.js
+++ FrontWeb/Webpack/1_基础/01_基本使用/src/main.js
@@ -3,6 +3,12 @@ import sum from "./js/sum";
 // 引入样式
 import "./css/index.css";
 import "./less/index.less";
+import "./sass/index.scss";
+import "./sass/index.sass";
+import "./stylus/index.styl";
+import "./css/iconfont.css";
 
-console.log(count(2, 1));
-console.log(sum(1, 2, 3, 4, 5, 6));
+const result1 = count(1, 2);
+const result2 = sum(1, 2, 3, 4, 5, 6);
+console.log(result1);
+console.log(result2);
