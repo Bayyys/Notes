c6df8a5b851bfaf6857f5d26cd138f004b482ab5
bayyy <475417309@qq.com>
2023-10-31 22:14:58 +0800

    📝 Project-React: Admin组件*
    1. LeftNav 组件
    
    bayyy-23.10.31

--- project/react/admin-client/src/components/LeftNav/LeftNav.jsx
+++ project/react/admin-client/src/components/LeftNav/LeftNav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "antd";
 
 import "./LeftNav.css";
@@ -9,6 +9,8 @@ import menuList from "../../config/menuConfig";
 import { getMenuNodes } from "../../utils/menuUtils";
 
 export default function LeftNav() {
+  const selectKey = useLocation().pathname; // 当前请求的路径
+  const openKey = useLocation().pathname.split("/").slice(0, -1).join("/"); // 当前请求的路径的父路径
   return (
     <div className="left-nav">
       <Link className="left-nav-header">
@@ -16,8 +18,9 @@ export default function LeftNav() {
         <h1>后台管理</h1>
       </Link>
       <Menu
-        defaultSelectedKeys={["/home"]} // 默认选中
-        defaultOpenKeys={["/products"]} // 默认展开
+        selectedKeys={[selectKey]} // 当前选中的菜单项 key 数组
+        defaultSelectedKeys={[selectKey]} // 默认选中
+        defaultOpenKeys={[openKey]} // 默认展开
         mode="inline" // 菜单类型: vertical(垂直) | horizontal(水平) | inline(内嵌)
         theme="dark"
       >
