2e75e45350143b7bea1c408dcfac3a0d1974ac82
BAY <475417309@qq.com>
2023-11-15 15:00:26 +0800

    📐 算法: 数组-习题
    1. 链表
    
    BAY-23.11.15

---
+++ Algorithm/leetcode/18.4-sum.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=cpp
+ * @lcpr version=30109
+ *
+ * [18] 四数之和
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,0,-1,0,-2,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,2,2,2,2]\n8\n
+// @lcpr case=end
+
+ */
+
