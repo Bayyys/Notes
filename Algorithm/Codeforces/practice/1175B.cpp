da39f7b91447f18f2692a195c687a24771cbbf74
BAY <475417309@qq.com>
2024-02-23 00:04:17 +0800

    📐 Algorithm: Leetcode & CF
    1. Daily
    2. CF1175B
    3. CF1254C: ⚠️注意乘积后范围是否符合要求
    
    BAY-24.02.23

---
+++ Algorithm/Codeforces/practice/1175B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long int lli;
+typedef long long ll;
+const ll MAX = pow(2, 32);
+
+void solve(int n) {
+  stack<ll> s;  // 操作栈: 1) add -> 0 2) for n -> n
+  s.push(1);    // 初始化栈
+  // end -> 栈弹出
+  ll val = 0;  // 当前的值
+  while (n--) {
+    string str;
+    cin >> str;
+    if (str == "for") {
+      ll x;
+      cin >> x;
+      s.push(min(MAX, x * s.top()));
+    } else if (str == "end") {
+      s.pop();
+    } else if (str == "add") {
+      val += s.top();
+    }
+  }
+  if (val >= MAX)
+    cout << "OVERFLOW!!!" << endl;
+  else
+    cout << val << endl;
+}
+
+int main() {
+  fio;
+  int n;
+  cin >> n;
+  solve(n);
+  return 0;
+}
--- Algorithm/leetcode/practice/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ Algorithm/leetcode/practice/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -22,6 +22,24 @@ using namespace std;
 #include <unordered_set>
 #include <utility>
 #include <vector>
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
 // @lcpr-template-end
 // @lc code=start
 /**
