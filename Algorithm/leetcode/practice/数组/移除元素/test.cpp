933d7c79c2c0112c5b3c32bfdecc887a348b4da4
bayyy <475417309@qq.com>
2024-03-06 17:35:04 +0800

    ⬆️ Update Repo Structure
    bayyy-24.03.06

--- Algorithm/leetcode/practice/数组/移除元素/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main() {
-    int a = 0;
-    return 0;
-}
---
+++ Tools/UploadLimit/LargeFile.py
@@ -0,0 +1,21 @@
+import os
+
+
+# 检查所有文件输出体积大于指定大小的文件
+def check_file_size(dir: str = "../..", file_size: int = 50):
+    file_size = file_size * 1024 * 1024  # MB to B
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if os.path.getsize(file_path) > file_size:
+                print(
+                    f"File: {file_path} size is {os.path.getsize(file_path) / 1024 / 1024} MB"
+                )
+        for dir in dirs:
+            if dir.find("node_modules") == -1:
+                continue
+            check_file_size(dir, file_size)
+
+
+if __name__ == "__main__":
+    check_file_size("../../")
