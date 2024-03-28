59893eabe97039f38715d3b05c43bdef5b2e7cd5
bayyy <475417309@qq.com>
2024-03-28 18:02:53 +0800

    📝 vim-plugins
    1. surround
    2. easymotion
    3. sneak

---
+++ Tools/vim/key_test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main() {
+  string s1 = "Hello, World!";
+  string s2 = "Hello, World!";
+  string s3 = "Hello, World!";
+  string s4 = "Hello, World!";
+  string s5 = "Hello, World!";
+  cout << "Hello, World!" << endl;
+  int a[5] = {1, 2, 3, 4, 5};
+  return 0;
+}
